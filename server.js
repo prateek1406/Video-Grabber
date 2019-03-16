b3631d21aa5cd802450b182ee0d92721d93d686e
Prateek Agarwal <prateek15jun@gmail.com>
2019-03-17 02:27:30 +0530

    Updated all files

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var ytdl = require('ytdl-core');
+
+
+const port =3000;
+
+var app = express();
+app.use(express.static(__dirname + '/views') );
+app.set('view engine', hbs);
+
+
+
+
+app.get('/', (req,res) => {
+    res.render('index.hbs');
+});
+
+
+connection((status) => {
+    if (status){
+        app.get('/download', (req,res) => {
+            res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+            ytdl(req.query.video, {
+                format: 'mp4'
+            }).pipe(res);
+        });
+    }
+    else{
+        app.get("/download", (req, res) => {
+            res.render("error.hbs", {
+                error : "Internet not connected"
+            })
+        })
+    }
+})
+
+
+function connection(callback){
+    require('dns').lookup('google.com', (error) => {
+        if (error){
+          
+            callback(false)
+        }
+        else{
+            callback(true);
+        }
+    })
+}
+
+
+app.listen(port, () => {
+    console.log('Server is up at port ' + port);
+});
+
